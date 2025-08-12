73496185d883c18a6e2bd1d1b05a5f4411125149
Simon Dirks <mail@simondirks.com>
2025-08-12 13:38:17 +0200

    Init basic Mirador viewer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// @ts-ignore
+import Mirador from 'mirador/dist/es/src/index';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected title = 'mirador-performance';
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(async () => {
+      const manifestUrl =
+        'https://iiif.io/api/cookbook/recipe/0001-mvm-image/manifest.json';
+
+      const config = {
+        id: 'mirador',
+
+        windows: [
+          {
+            manifestId: manifestUrl,
+          },
+        ],
+      };
+
+      const miradorInstance = Mirador.viewer(config);
+    });
+  }
 }
